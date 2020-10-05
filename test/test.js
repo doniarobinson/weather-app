2019fd2e38a675aeba8d3bfd72695c4a99e37d6d
doniarobinson <doniamae@gmail.com>
2020-10-05 12:07:18 -0500

    Additional updates for .env change

--- test/test.js
+++ test/test.js
@@ -4,7 +4,9 @@ const chaiHttp = require('chai-http');
 
 chai.use(chaiHttp);
 
-// not ideal to put the API key here, but with more time, I'd set up a proper environment!
+// tester should fill a real api key in here
+let api_key = 'abc123';
+
 describe("API", () => {
   it("is not authorized", done => {
     chai.request('https://api.openweathermap.org')
@@ -16,7 +18,7 @@ describe("API", () => {
   });
   it("is authorized", done => {
     chai.request('https://api.openweathermap.org')
-      .get("/data/2.5/onecall?lat=41.8781&lon=-87.6298&units=imperial&exclude=minutely&APPID=a5d0020f1e0212bc37def4492b2218ba")
+      .get(`/data/2.5/onecall?lat=41.8781&lon=-87.6298&units=imperial&exclude=minutely&APPID=${api_key}`)
       .end((err, res) => {
         expect(res).to.have.status(200);
         done();
@@ -24,7 +26,7 @@ describe("API", () => {
   });
   it("gets multiple days of temperatures", done => {
     chai.request('https://api.openweathermap.org')
-      .get("/data/2.5/onecall?lat=41.8781&lon=-87.6298&units=imperial&exclude=minutely&APPID=a5d0020f1e0212bc37def4492b2218ba")
+      .get(`/data/2.5/onecall?lat=41.8781&lon=-87.6298&units=imperial&exclude=minutely&APPID=${api_key}`)
       .end((err, res) => {
         expect(Object.keys(res.body.daily)).to.have.lengthOf(8);
         done();
