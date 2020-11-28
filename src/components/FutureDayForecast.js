c73b01a493d59b24ba06d00e2009925e36df828f
doniarobinson <doniamae@gmail.com>
2020-11-28 16:03:00 -0600

    Change FutureDayForecast from class to function

--- src/components/FutureDayForecast.js
+++ src/components/FutureDayForecast.js
@@ -1,30 +1,23 @@
 import React from 'react';
 
-class FutureDayForecast extends React.Component {
-
-  render() {
-    const forecast = this.props.forecast;
-
-    function getImgUrl(imgName) {
-      return "http://openweathermap.org/img/wn/" + imgName + "@2x.png";
-    }
-
-    function cleanUpTemp(temp) {
-      return Math.round(temp) + '°';
-    }
-
-    return (
-      <div className="day rounded-lg">
-        <h3>{this.props.day}</h3>
-
-        <p><img src={getImgUrl(forecast.weather[0].icon)} alt={forecast.weather[0].description} /></p>
-
-        <h4>High: {cleanUpTemp(forecast.temp.max)}</h4>
-
-        <h4>Low: {cleanUpTemp(forecast.temp.min)}</h4>
-      </div>
-    );
+export default function FutureDayForecast({ day, forecast }) {
+  function cleanUpTemp(temp) {
+    return Math.round(temp) + '°';
   }
-}
 
-export default FutureDayForecast;
+  function getImgUrl(imgName) {
+    return `http://openweathermap.org/img/wn/${imgName}@2x.png`;
+  }
+
+  return (
+    <div className="day rounded-lg">
+      <h3>{day}</h3>
+
+      <p><img src={getImgUrl(forecast.weather[0].icon)} alt={forecast.weather[0].description} /></p>
+
+      <h4>High: {cleanUpTemp(forecast.temp.max)}</h4>
+
+      <h4>Low: {cleanUpTemp(forecast.temp.min)}</h4>
+    </div>
+  );
+}
